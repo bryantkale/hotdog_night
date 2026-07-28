3af246e400d80954428e4605b848cb93dd142650
Caelin Bryant <caelinbryant@gmail.com>
2026-07-28 15:01:22 -0400

    Fix to work for modern browsers

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        emptyOutDir: true,
+    },
     server: {
         host: "0.0.0.0",
         port: 3000,
